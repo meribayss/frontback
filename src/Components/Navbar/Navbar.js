04480870d2e5d7df631c326a796c16f983286185
Мээрим <meerim@MacBook-Air-Meerim.local>
2022-06-14 17:53:29 +0600

    ok

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -21,7 +21,7 @@ const Navbar = () => {
   return (
     <div className={color ? "header header-bg" : "header"}>
       <Link to="/">
-        <h1>GLX TRVL</h1>
+        <h1>Timpom</h1>
       </Link>
       <ul className={click ? "nav-menu active" : "nav-menu"}>
         <li>
