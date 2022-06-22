9fca99756da861223c804ebe08c525afd9104647
Kamila Irsalieva <kamilairsalieva@Kamilas-MacBook-Air.local>
2022-06-23 05:42:07 +0600

    blya

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -47,49 +47,51 @@ const Login = () => {
   }
 
   return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <div className="card" style={{ width: "50vw" }}>
-        <div className="card-body">
-          <h2>Login</h2>
-          <div className="mb-3">
-            <input
-              type="text"
-              className="form-control"
-              size={"200px"}
-              onChange={(e) => setEmail(e.target.value)}
-              name="email"
-              key="email"
-              placeholder="*Email"
-            />
+    <div className="container-login">
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div className="card" style={{ width: "50vw" }}>
+          <div className="card-body">
+            <h2>Login</h2>
+            <div className="mb-3">
+              <input
+                type="text"
+                className="form-control"
+                size={"200px"}
+                onChange={(e) => setEmail(e.target.value)}
+                name="email"
+                key="email"
+                placeholder="*Email"
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                type="password"
+                className="form-control"
+                onChange={(e) => setPassword(e.target.value)}
+                name="password"
+                key="password"
+                placeholder="*Password"
+              />
+            </div>
+            <NavLink to="/">
+              <button
+                onClick={handleClick}
+                className="btn btn-light"
+                style={{ width: "100%" }}
+              >
+                Login
+              </button>
+              <ToastContainer />
+            </NavLink>
           </div>
-          <div className="mb-3">
-            <input
-              type="password"
-              className="form-control"
-              onChange={(e) => setPassword(e.target.value)}
-              name="password"
-              key="password"
-              placeholder="*Password"
-            />
-          </div>
-          <NavLink to="/">
-            <button
-              onClick={handleClick}
-              className="btn btn-light"
-              style={{ width: "100%" }}
-            >
-              Login
-            </button>
-            <ToastContainer />
-          </NavLink>
         </div>
       </div>
     </div>
